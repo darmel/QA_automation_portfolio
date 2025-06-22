f943ee5658f02f3a71c678a5c76ccf9d42757d6a
Dario OCHOA <dario.martin.8a@gmail.com>
2025-06-21 22:33:16 -0300

    chroe: fix report_publish.sh

--- common/scripts/report_publish.sh
+++ common/scripts/report_publish.sh
@@ -5,17 +5,28 @@ set -e  # stop if something goes wrong...
 REPORT_REPO="/var/jenkins_home/parabank_tests_reports"
 SOURCE_REPORT="/var/jenkins_home/workspace/parabank_API_and_Front/allure-report"
 FECHA=$(date +'%Y%m%d_%H%M')
+echo "varaibles declaradas"
 
 # move to reports repo
 cd "$REPORT_REPO"
+pwd
 
 #clean old data except .git and .gitignore
-find . -mindepth 1 ! -name '.git' ! -name '.gitignore' -exec rm -rf {} +
+rsync -av --delete --exclude='.git' --exclude='.gitignore' "$SOURCE_REPORT"/ .
+
+echo "data cleaned"
+ls -las
 
 #copi new data
 cp -r "$SOURCE_REPORT"/* "$REPORT_REPO"
+echo "new data copieda"
+#ls -las
 
 # git add, commit and push
+pwd
+git status
 git add .
 git commit -m "Actualización automática del reporte $FECHA"
 git push
+
+echo "publish reports script finish"
